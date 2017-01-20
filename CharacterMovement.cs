a6a73b0acb7fe4e9dda5a93286d63f0f10718565
Jarrett vdB <jsvanden@users.noreply.github.com>
2017-01-20 12:49:26 -0800

    Add files via upload

---
+++ CharacterMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class CharacterMovement : MonoBehaviour
+{
+	private Rigidbody2D rigidBody;
+
+	[Header("Movement")]
+	public float jumpForce = 20.0f;
+	public float moveSpeed = 10.0f;
+
+	[Header("Utility")]
+	public float groundCheckDistance = 1.0f;
+
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		float horizontalInput = Input.GetAxisRaw ("Horizontal");
+		Vector2 moveDirection = new Vector2 (horizontalInput * moveSpeed, 0);
+		rigidBody.AddForce (moveDirection);
+
+		//print (isGrounded());
+
+		if(Input.GetButtonDown("Jump") && isGrounded())
+		{
+			rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+		}
+	}
+
+
+	bool isGrounded()
+	{
+		Vector2 groundCheckPos = new Vector2 (transform.position.x, transform.position.y - groundCheckDistance);
+
+		bool result =  Physics2D.Linecast(transform.position, groundCheckPos);
+
+		if (result)
+		{
+			Debug.DrawLine(transform.position, groundCheckPos, Color.green);
+		}
+		else
+		{
+			Debug.DrawLine(transform.position, groundCheckPos, Color.red);
+		}
+
+		return result;
+	}
+}
---
+++ FPMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class FPMovement : MonoBehaviour
+{
+	private Transform mainCam;
+	private CharacterController characterController;
+
+	private float mouseX = 0.0f;
+	private float mouseY = 0.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	[Header("Mouse Sensitivity")]
+	public float mouseSpeedX = 2.0f;
+	public float mouseSpeedY = 2.0f;
+
+	[Header("Movement")]
+	public float moveSpeed = 6.0f;
+	public float jumpSpeed = 8.0f;
+
+	[Header("Gravity")]
+	public float gravity = 20.0f;
+
+	void Start ()
+	{
+		mainCam = Camera.main.transform;
+		characterController = this.GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		// MOUSE
+
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+		mouseX += mouseSpeedX * Input.GetAxis("Mouse X");
+		transform.eulerAngles = new Vector3(0.0f, mouseX, 0.0f);
+
+		mouseY -= mouseSpeedY * Input.GetAxis("Mouse Y");
+		mouseY = Mathf.Clamp (mouseY, -90, 90);
+
+		mainCam.transform.eulerAngles = new Vector3(mouseY, mainCam.eulerAngles.y, 0.0f);
+
+		// MOVEMENT
+
+		if(characterController.isGrounded)
+		{
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= moveSpeed;
+
+			RaycastHit hitInfo;
+			Physics.SphereCast(transform.position, characterController.radius, Vector3.down, out hitInfo, characterController.height/2f);
+			moveDirection = Vector3.ProjectOnPlane(moveDirection, hitInfo.normal);
+
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+		}
+
+		// GRAVITY
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		characterController.Move(moveDirection * Time.deltaTime);
+	}
+}
